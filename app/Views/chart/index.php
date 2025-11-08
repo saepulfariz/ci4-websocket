d6cb4923c9f674b9ed20b40482b1c309d33693cd
saepulfariz <saepulfariz108@gmail.com>
2025-11-09 06:02:44 +0700

    change style view chart

--- app/Views/chart/index.php
+++ app/Views/chart/index.php
@@ -1,14 +1,58 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Realtime Chart</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            font-family: sans-serif;
+        }
+
+        .chartMenu {
+            width: 100vw;
+            height: 40px;
+            background: #1A1A1A;
+            color: rgba(54, 162, 235, 1);
+        }
+
+        .chartMenu p {
+            padding: 10px;
+            font-size: 20px;
+        }
+
+        .chartCard {
+            width: 100vw;
+            height: calc(100vh - 40px);
+            background: rgba(54, 162, 235, 0.2);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .chartBox {
+            width: 700px;
+            padding: 20px;
+            border-radius: 20px;
+            border: solid 3px rgba(54, 162, 235, 1);
+            background: white;
+        }
+    </style>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
 <body>
-    <h2>Realtime Chart dengan WebSocket</h2>
-    <canvas id="myChart" width="400" height="200"></canvas>
+    <div class="chartMenu">
+        <p>Realtime Chart dengan WebSocket (Chart JS <span id="chartVersion"></span>)</p>
+    </div>
+    <div class="chartCard">
+        <div class="chartBox">
+            <canvas id="myChart"></canvas>
+        </div>
+    </div>
 
     <script>
         const ctx = document.getElementById('myChart').getContext('2d');
